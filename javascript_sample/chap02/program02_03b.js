2e591cf584df630ab41d6603dd5cad0ae41e1d1d
musuko <100979661+musuko@users.noreply.github.com>
2026-07-17 02:57:19 +0000

    サンプルプログラム

---
+++ javascript_sample/chap02/program02_03b.js
@@ -0,0 +1,12 @@
+// program02_03.js: 変数、代入、データ型
+// var x, y, z;
+var x=100;
+var y=23;
+var z=x+y;
+// x + y は数値計算となり 123 になる
+console.log("x=",x," y=",y," z=",z);
+x="100";
+y="23";
+z=x+y;
+// x + y は文字の結合になり "10023" になる
+console.log("x=",x," y=",y," z=",z);
---
+++ javascript_sample/chap05/server05_01v6.js
@@ -0,0 +1,18 @@
+// server05_01.js - IPV6 対応
+// 簡易 Web サーバー サンプル
+// なにがあっても、Hello! World! しか返さない!
+
+// 開始メッセージ
+console.log('server05_01 開始')
+
+// http サーバー
+var http1 = require('http'); // 受信プログラムを設定
+http1.createServer(          // 受信したら動作する
+  // メッセージを処理する無名関数
+  function (req, msg1) {
+    msg1.writeHead(200, {'Content-Type': 'text/plain'});
+    msg1.end('Hello! Good bye!\n');
+  }
+).listen(8080, '::1');  // IP とポートを設定する
+// url を表示しておく(動作とは無関係)
+console.log('http://[::1]:8080/index.html')
